b036594b1376cbb38bd1e8fc3420ccfc2d720632
stuh <stuart@northcolour.com>
2025-10-21 13:34:29 +0100

    remove map marker pin from addresses

--- dist/aliss-map.js
+++ dist/aliss-map.js
@@ -319,9 +319,6 @@ const buildServiceCard = (service, locationOverride) => {
       <a href="Javascript:;" tabindex="0" data-markerid="${service.id}${location.latitude}${location.longitude}">
         ${location.formatted_address}
       </a> 
-      <svg class="pinsvg" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-        <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
-      </svg>
     </span>`;
   });
   serviceLocations.innerHTML = locationsHTML;
@@ -892,9 +889,7 @@ style.innerHTML = `
         padding:5px 0px 5px 0;
         margin-right:10px;
       }
-      .aliss-map .location-map-link .pinsvg{
-        display:none;
-      }
+      
       .aliss-map .results-list {
         max-height: 75vh;
         overflow-y: auto;
@@ -918,10 +913,6 @@ style.innerHTML = `
         display:block;
       }
       
-      .aliss-map .results-list .location-map-link .pinsvg{
-        display:inline-block;
-        margin-right:7px;
-      }
       .aliss-map .results-list .location-map-link{
         cursor:pointer;
         display:flex;
